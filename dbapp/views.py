da63d1eb74a2f058b2726217df4cdf7da1abd8a3
globalista <globalista@seznam.cz>
2020-03-10 23:08:13 +0100

    Make domain_view and domains_view for data from domain table

--- dbapp/views.py
+++ dbapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Domain
 # Create your views here.
 from django.http import HttpResponse
 
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the db_app index.")
 def index(request):
-    return HttpResponse("Hello, world. You're at the db_app index.")
+    domains = Domain.objects.select_related('domain_flag')
+    return render(request, 'dbapp/domains_view.html', {'domains': domains})
+
+def domain_view(request, pk):
+    domain = Domain.objects.filter(id=pk)
+    return render(request, 'dbapp/domain_view.html', {'domain': domain})
+

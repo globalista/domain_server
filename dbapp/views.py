82e7493c7c937357d52713a7a32a0bc6c10b2da2
globalista <globalista@seznam.cz>
2020-03-11 23:34:44 +0100

    completed views

--- dbapp/views.py
+++ dbapp/views.py
@@ -7,17 +7,25 @@ def active_flag(valid_from, valid_to):
         valid_to = datetime.max
     return valid_from < datetime.now() < valid_to
 
+def make_list_of_active_flags(domain_flags):
+    valid_domain_flags = []
+    for i in domain_flags:
+        if active_flag(i.valid_from, i.valid_to):
+            valid_domain_flags.append(i)
+    return valid_domain_flags
+
 
 def index(request):
     domains = Domain.objects.all()
-    return render(request, 'dbapp/domains_view.html', {'domains': domains})
+    domain_flags = DomainFlag.objects.all()
+    valid_domain_flags = make_list_of_active_flags(domain_flags)
+    return render(request, 'dbapp/domains_view.html', {'domains': domains, 'domain_flags': valid_domain_flags})
+
+
 
 def domain_view(request, pk):
     domain0 = Domain.objects.filter(id=pk)[0]
-    domain_flag = DomainFlag.objects.filter(domain = domain0)
-    valid_domain_flag = []
-    for i in domain_flag:
-        if active_flag(i.valid_from, i.valid_to):
-            valid_domain_flag.append(i)
-    return render(request, 'dbapp/domain_view.html', {'domain': domain0, 'domain_flag': valid_domain_flag})
+    domain_flags = DomainFlag.objects.filter(domain = domain0)
+    valid_domain_flags = make_list_of_active_flags(domain_flags)
+    return render(request, 'dbapp/domain_view.html', {'domain': domain0, 'domain_flag': valid_domain_flags})
 

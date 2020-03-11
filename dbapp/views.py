e54eb6daae2e89ab3d6e63a0d6c1ef529cb909fd
globalista <globalista@seznam.cz>
2020-03-11 22:28:42 +0100

    complete one domain view

--- dbapp/views.py
+++ dbapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .models import Domain, DomainFlag
+from datetime import datetime
+
+def active_flag(valid_from, valid_to):
+    if not valid_to:
+        valid_to = datetime.max
+    return valid_from < datetime.now() < valid_to
 
 
 def index(request):
@@ -9,5 +15,9 @@ def index(request):
 def domain_view(request, pk):
     domain0 = Domain.objects.filter(id=pk)[0]
     domain_flag = DomainFlag.objects.filter(domain = domain0)
-    return render(request, 'dbapp/domain_view.html', {'domain': domain0, 'domain_flag': domain_flag})
+    valid_domain_flag = []
+    for i in domain_flag:
+        if active_flag(i.valid_from, i.valid_to):
+            valid_domain_flag.append(i)
+    return render(request, 'dbapp/domain_view.html', {'domain': domain0, 'domain_flag': valid_domain_flag})
 
